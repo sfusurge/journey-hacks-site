c213b4de4281a13f8dd38434e565e68ea27d0802
Christina <christinaraganit@gmail.com>
2024-12-27 21:10:49 -0800

    added the ledge lol

---
+++ src/lib/components/MainPageParts/conveyorBelt.svelte
@@ -0,0 +1,11 @@
+<div class="bg-[#754f4f]">
+  <div
+    class="scroller bg-[#754f4f] min-w-[100vw] max-w-[100vw]"
+    data-direction="right"
+    data-speed="slow"
+  ></div>
+  <img
+    src="/conveyor-belt/ledge.png"
+    class="h-48 object-cover w-screen max-w-screen min-w-screen object-bottom pointer-events-none"
+  />
+</div>
