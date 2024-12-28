1c1769aa54430c44cb6899e800af3e8491698430
Christina <christinaraganit@gmail.com>
2024-12-27 19:23:49 -0800

    footer done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
-    import Example from "$lib/components/example.svelte";
-    import Home from "$lib/components/MainPageParts/home.svelte";
-    import Apply from "$lib/components/MainPageParts/apply.svelte";
-    import About from "$lib/components/MainPageParts/about.svelte";
-    import Faq from "$lib/components/MainPageParts/faq.svelte";
-    import InfoBook from "$lib/components/MainPageParts/InfoBook.svelte";
+  import Example from "$lib/components/example.svelte";
+  import Home from "$lib/components/MainPageParts/home.svelte";
+  import Apply from "$lib/components/MainPageParts/apply.svelte";
+  import About from "$lib/components/MainPageParts/about.svelte";
+  import Faq from "$lib/components/MainPageParts/faq.svelte";
+  import InfoBook from "$lib/components/MainPageParts/InfoBook.svelte";
+  import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <Home />
 <Apply />
 <About />
 <Faq />
-<InfoBook/>
+<InfoBook />
+<Footer />
