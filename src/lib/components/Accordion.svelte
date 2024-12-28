fc82c100d17048e62b3ae9167b503ce373dced66
goldentoaste <rayg2375@gmail.com>
2024-12-27 18:15:17 -0800

    scripting for accordian

--- src/lib/components/Accordion.svelte
+++ src/lib/components/Accordion.svelte
@@ -1,50 +1,39 @@
 <script lang="ts">
-    let {question, answer} = $props();
-    let isOpen = $state(false); 
+    let { question, answer } = $props();
+    let isOpen = $state(false);
     let innerHeight = $state(0);
-
-    $inspect(innerHeight);
 </script>
 
 <div class="accordion">
-    <button class="button" onclick={() => isOpen = !isOpen} >
-      {question}
+    <button class="button" onclick={() => (isOpen = !isOpen)}>
+        {question}
     </button>
 
-    <div class="answer-container" style={`maxHeight: ${(isOpen? innerHeight : 0)}px`}> 
-      <div class="answer" bind:clientHeight={innerHeight} class:closed={!isOpen} class:open={isOpen}>
-        {answer}
-      </div>
-    </div> 
+    <div class="answer-container" style="height:{innerHeight}px; max-height: {isOpen ? innerHeight : 0}px;">
+        <div class="answer" bind:clientHeight={innerHeight}>
+            {answer}
+        </div>
+    </div>
 </div>
- 
+
 <style>
     .accordion {
-        position: relative;
+        height: fit-content;
     }
 
     .answer-container {
-        max-height: 0px;
+        position: relative;
         width: 100%;
         background-color: red;
-        overflow: hidden;
         transition: max-height 0.2s ease-out;
+        overflow: hidden;
+        max-height: 0px;
     }
 
     .answer {
         position: absolute;
         width: 100%;
-    }
-
-    .closed {
-        padding: 0 18px;
-        background-color: white;
-        max-height: 0;
-        overflow: hidden;
-    }
-
-    .open {
-
+        left:0;
+        top: 0;
     }
 </style>
-
