b001bc0ad207310df4d4f2100f65c262a4739d90
goldentoaste <rayg2375@gmail.com>
2024-12-27 16:06:17 -0800

    finished book integrations

--- src/lib/components/MainPageParts/InfoBook.svelte
+++ src/lib/components/MainPageParts/InfoBook.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import Book from "$lib/components/book/Book.svelte";
+    import StickyLabel from "../StickyLabel.svelte";
     const imgUrls = [...Array(7).keys()].map((idx) => `/bookpages/${idx + 1}.webp`);
     const backPageUrl = "/bookpages/0.webp";
 
@@ -7,6 +8,7 @@
 </script>
 
 <div class="bookContainer" bind:clientWidth={screenWidth}>
+   <div class="labelContainer"> <StickyLabel>THE CHEFS</StickyLabel></div>
     <Book backUrl={backPageUrl} {imgUrls} singlePageMode={screenWidth < 1200}></Book>
 </div>
 
@@ -16,6 +18,10 @@
         height: fit-content;
         padding: 2rem;
 
-        background-color: #DFB19B;
+        background-color: #dfb19b;
+    }
+
+    .labelContainer{
+        margin-left: 3rem;
     }
 </style>
---
+++ src/lib/components/StickyLabel.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    let { children } = $props();
+</script>
+
+<div class="labelContainer">
+    {@render children()}
+</div>
+
+<style>
+    .labelContainer {
+        background-image: url("/StickerLabel.svg");
+        background-color: transparent;
+        /* strech image to fill */
+        background-size: 100% 100%;
+
+        color: #835242;
+        font-size: 50px;
+        font-weight: 500;
+        font-family: "Coiny", sans-serif;
+        width: fit-content;
+ 
+
+        line-height: 100%;
+        padding:3rem;
+
+        transform: rotate(-7deg);
+    }
+</style>
