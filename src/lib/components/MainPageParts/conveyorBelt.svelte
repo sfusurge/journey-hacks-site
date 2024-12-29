491031f333c928a233d26e229d7d494cb6eebc16
Christina <christinaraganit@gmail.com>
2024-12-28 19:37:26 -0800

    more conveyor belt

--- src/lib/components/MainPageParts/conveyorBelt.svelte
+++ src/lib/components/MainPageParts/conveyorBelt.svelte
@@ -33,7 +33,7 @@
 
 <div class="h-8 w-full bg-[#754f4f]"></div>
 
-<div class="relative w-full h-full">
+<div class="relative w-full h-full max-w-[100vw]">
   <img
     alt="background with ledge"
     src="/conveyor-belt/background-with-ledge.png"
@@ -46,6 +46,66 @@
       src="/conveyor-belt/foreground.png"
       class="relative w-full h-auto top-[49.3902%] z-30"
     />
+
+    <div
+      class="scroll absolute top-[5.8%] z-50 w-screen max-w-[100vw] overflow-hidden"
+    >
+      <div class="m-scroll flex gap-[4%]">
+        <img
+          alt="food 0"
+          src="/conveyor-belt/food-0.png"
+          class="w-[11.1656%] h-auto"
+        />
+
+        <img
+          alt="food 1"
+          src="/conveyor-belt/food-1.png"
+          class="w-[11.1656%] h-auto"
+        />
+
+        <img
+          alt="food 2"
+          src="/conveyor-belt/food-2.png"
+          class="w-[11.1656%] h-auto"
+        />
+
+        <img
+          alt="food 3"
+          src="/conveyor-belt/food-3.png"
+          class="w-[11.1656%] h-auto"
+        />
+
+        <img
+          alt="food 4"
+          src="/conveyor-belt/food-4.png"
+          class="w-[11.1656%] h-auto"
+        />
+
+        <img
+          alt="food 5"
+          src="/conveyor-belt/food-5.png"
+          class="w-[11.1656%] h-auto"
+        />
+
+        <img
+          alt="food 6"
+          src="/conveyor-belt/food-6.png"
+          class="w-[11.1656%] h-auto"
+        />
+
+        <img
+          alt="food 7"
+          src="/conveyor-belt/food-7.png"
+          class="w-[11.1656%] h-auto"
+        />
+
+        <img
+          alt="food 8"
+          src="/conveyor-belt/food-8.png"
+          class="w-[11.1656%] h-auto"
+        />
+      </div>
+    </div>
 
     <img
       alt="ledge"
@@ -62,49 +122,64 @@
     <img
       alt="wheel 1"
       src="/conveyor-belt/wheel-2.png"
-      class="absolute left-[2.679%] top-[41.4674%] w-[5.834%] h-auto auto z-20"
+      class="absolute left-[2.679%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 2"
       src="/conveyor-belt/wheel-2.png"
-      class="absolute left-[16.2520%] top-[41.4674%] w-[5.834%] h-auto auto z-20 rotate-12"
+      class="absolute left-[16.2520%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 3"
       src="/conveyor-belt/wheel-2.png"
-      class="absolute left-[29.825%] top-[41.4674%] w-[5.834%] h-auto auto z-20 rotate-45"
+      class="absolute left-[29.825%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 4"
       src="/conveyor-belt/wheel-2.png"
-      class="absolute left-[43.3984%] top-[41.4674%] w-[5.834%] h-auto auto z-20 rotate-90"
+      class="absolute left-[43.3984%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 5"
       src="/conveyor-belt/wheel-2.png"
-      class="absolute left-[50.728%] top-[41.4674%] w-[5.834%] h-auto auto z-20 rotate-12"
+      class="absolute left-[50.728%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 6"
       src="/conveyor-belt/wheel-2.png"
-      class="absolute left-[64.301%] top-[41.4674%] w-[5.834%] h-auto auto z-20 rotate-45"
+      class="absolute left-[64.301%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 7"
       src="/conveyor-belt/wheel-2.png"
-      class="absolute left-[77.8739%] top-[41.4674%] w-[5.834%] h-auto auto z-20 rotate-90"
+      class="absolute left-[77.8739%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 8"
       src="/conveyor-belt/wheel-2.png"
-      class="absolute left-[91.44689%] top-[41.4674%] w-[5.834%] h-auto auto z-20 rotate-12"
+      class="absolute left-[91.44689%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
   </div>
 </div>
+
+<style>
+  .m-scroll {
+    animation: scroll 15s linear infinite;
+  }
+
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(-100%);
+    }
+  }
+</style>
