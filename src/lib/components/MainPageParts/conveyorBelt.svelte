0e5874d5a276088d5757b54bba42bc0094bba895
goldentoaste <rayg2375@gmail.com>
2024-12-30 13:21:47 -0800

    converted conveyor belt assets to webp format

--- src/lib/components/MainPageParts/conveyorBelt.svelte
+++ src/lib/components/MainPageParts/conveyorBelt.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
   const imageUrls = [
     // TODO switch to .svg later.
-    "food-0.png",
-    "food-1.png",
-    "food-2.png",
-    "food-3.png",
-    "food-4.png",
-    "food-5.png",
-    "food-6.png",
-    "food-7.png",
-    "food-8.png",
+    "food-0.webp",
+    "food-1.webp",
+    "food-2.webp",
+    "food-3.webp",
+    "food-4.webp",
+    "food-5.webp",
+    "food-6.webp",
+    "food-7.webp",
+    "food-8.webp",
   ];
 
   let shownImages: { url: string; imgId: number }[] = $state([]);
@@ -48,18 +48,21 @@
 <div class="relative w-full h-full max-w-[100vw]">
   <img
     alt="background with ledge"
-    src="/conveyor-belt/background-with-ledge.png"
+    src="/conveyor-belt/background-with-ledge.webp"
     class="relative w-full h-auto"
   />
 
   <div class="absolute top-0 left-0 w-full h-full">
     <img
       alt="foreground"
-      src="/conveyor-belt/foreground.png"
+      src="/conveyor-belt/foreground.webp"
       class="relative w-full h-auto top-[49.3902%] z-30"
     />
 
-    <div class="scroll absolute top-[5.8%] z-50 overflow-hidden h-full" style=" width: 100%; max-width: 100dvw;">
+    <div
+      class="scroll absolute top-[5.8%] z-50 overflow-hidden h-full"
+      style=" width: 100%; max-width: 100dvw;"
+    >
       {#each shownImages as foodInfo (foodInfo.imgId)}
         <img src="/conveyor-belt/{foodInfo.url}" alt="bleh" class="m-scroll w-[11.1656%] h-auto" />
       {/each}
@@ -67,61 +70,61 @@
 
     <img
       alt="ledge"
-      src="/conveyor-belt/ledge.png"
+      src="/conveyor-belt/ledge.webp"
       class="absolute bottom-0 w-full h-auto auto z-40"
     />
 
     <img
       alt="background"
-      src="/conveyor-belt/background.png"
+      src="/conveyor-belt/background.webp"
       class="absolute top-[29.0650%] w-full h-auto auto z-0"
     />
 
     <img
       alt="wheel 1"
-      src="/conveyor-belt/wheel-2.png"
+      src="/conveyor-belt/wheel-2.webp"
       class="absolute left-[2.679%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 2"
-      src="/conveyor-belt/wheel-2.png"
+      src="/conveyor-belt/wheel-2.webp"
       class="absolute left-[16.2520%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 3"
-      src="/conveyor-belt/wheel-2.png"
+      src="/conveyor-belt/wheel-2.webp"
       class="absolute left-[29.825%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 4"
-      src="/conveyor-belt/wheel-2.png"
+      src="/conveyor-belt/wheel-2.webp"
       class="absolute left-[43.3984%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 5"
-      src="/conveyor-belt/wheel-2.png"
+      src="/conveyor-belt/wheel-2.webp"
       class="absolute left-[50.728%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 6"
-      src="/conveyor-belt/wheel-2.png"
+      src="/conveyor-belt/wheel-2.webp"
       class="absolute left-[64.301%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 7"
-      src="/conveyor-belt/wheel-2.png"
+      src="/conveyor-belt/wheel-2.webp"
       class="absolute left-[77.8739%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
 
     <img
       alt="wheel 8"
-      src="/conveyor-belt/wheel-2.png"
+      src="/conveyor-belt/wheel-2.webp"
       class="absolute left-[91.44689%] top-[41.4674%] w-[5.834%] h-auto auto z-20 animate-spin"
     />
   </div>
