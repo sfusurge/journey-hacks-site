df121ebaa687230ad3ca102cd594b4e10c0727a5
goldentoaste <rayg2375@gmail.com>
2025-01-02 14:36:40 -0800

    trying a diff reload strat, SEO, and site preview

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,18 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<!-- to preload font and image to be used in front page -->
+	<link rel="preload" href="/bg.webp" as="image" />
+	<link rel="preload" href="/fonts/coiny.woff2" type="font/woff2" as="font" crossorigin="anonymous"/>
+	<link rel="preload" href="/fonts/neue_medium.woff" type="font/woff" as="font" crossorigin="anonymous"/>
+
+	<title>Journey Hacks 2025</title>
+	<meta name="description" content="Journey Hacks 2025, hosted by SFU Surge"/>
+
+	<meta property="og:title" content="Journey Hacks 2025"/>	
+	<meta property="og:description" content="JOURNEY HACKS JOURNEY HACK JOURNEY HACKS"/>	
+	<meta property="og:image" content="/bg.webp"/>	
+</svelte:head>
+
 {@render children()}
