064a4a7d2360619830dfabba4f6173fd7b487a94
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2025-02-10 14:47:15 -0800

    Updated sponsors component so that on breakpoint lg, the row of sponsor logos turns into two vertical columns

--- src/lib/components/MainPageParts/Sponsors.svelte
+++ src/lib/components/MainPageParts/Sponsors.svelte
@@ -9,31 +9,68 @@
             Our Sponsors
         </StickyLabel>
 
-        <div class="flex lg:flex-row flex-col items-center justify-center lg:gap-x-20 gap-y-10">
-            <SponsorButton
-                    href="https://www.perplexity.ai"
-                    src="/sponsor-logos/perplexity-logo.svg"
-                    alt="Perplexity Logo"
-            />
-            <SponsorButton
-                    href="https://www.redbull.com/ca-en"
-                    src="/sponsor-logos/redbull-logo.svg"
-                    alt="Redbull Logo"
-            />
-            <SponsorButton
-                    href="https://balsamiq.com/"
-                    src="/sponsor-logos/balsamiq-logo.png"
-                    alt="Balsamiq Logo"
-            />
-            <SponsorButton
-                    href="https://www.caterdash.com/"
-                    src="/sponsor-logos/caterdash-logo.svg"
-                    alt="Caterdash Logo"
-                    width="w-16"
-                    height="h-16"
-            />
+        <!-- Desktop only -->
+        <div class="hidden lg:block">
+            <div class="flex flex-row items-center justify-center lg:gap-x-20 gap-y-10">
+                <SponsorButton
+                        href="https://www.perplexity.ai"
+                        src="/sponsor-logos/perplexity-logo.svg"
+                        alt="Perplexity Logo"
+                />
+                <SponsorButton
+                        href="https://www.redbull.com/ca-en"
+                        src="/sponsor-logos/redbull-logo.svg"
+                        alt="Redbull Logo"
+                />
+                <SponsorButton
+                        href="https://balsamiq.com/"
+                        src="/sponsor-logos/balsamiq-logo.png"
+                        alt="Balsamiq Logo"
+                />
+                <SponsorButton
+                        href="https://www.caterdash.com/"
+                        src="/sponsor-logos/caterdash-logo.svg"
+                        alt="Caterdash Logo"
+                        width="w-16"
+                        height="h-16"
+                />
+            </div>
+        </div>
+
+        <!-- Mobile only -->
+        <div class="block lg:hidden">
+            <div class="flex flex-row items-center justify-center gap-x-20 gap-y-10">
+                <div class="flex flex-col items-center justify-center gap-y-10">
+                    <SponsorButton
+                            href="https://www.perplexity.ai"
+                            src="/sponsor-logos/perplexity-logo.svg"
+                            alt="Perplexity Logo"
+                    />
+                    <SponsorButton
+                            href="https://www.redbull.com/ca-en"
+                            src="/sponsor-logos/redbull-logo.svg"
+                            alt="Redbull Logo"
+                    />
+                </div>
+
+                <div class="flex flex-col items-center justify-center">
+                    <SponsorButton
+                            href="https://balsamiq.com/"
+                            src="/sponsor-logos/balsamiq-logo.png"
+                            alt="Balsamiq Logo"
+                    />
+                    <SponsorButton
+                            href="https://www.caterdash.com/"
+                            src="/sponsor-logos/caterdash-logo.svg"
+                            alt="Caterdash Logo"
+                            width="w-16"
+                            height="h-16"
+                    />
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
 
 <style></style>
